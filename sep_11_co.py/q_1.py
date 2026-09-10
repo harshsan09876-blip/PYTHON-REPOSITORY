c921e3697f92c077d46a109b83036b96eb69513b
Harsh Chauhan <harshsan09876@gmail.com>
2026-09-11 00:40:45 +0530

    how to generate the salry of employees when there bonus is upcoming

---
+++ sep_11_co.py/q_1.py
@@ -0,0 +1,12 @@
+#basic data input and string formatting
+
+a = (input("ENTER THE NAME OF PERSON:"))
+salary = (input("ENTER THE SALARY(IN $): "))
+POPULATION_INDIA = (input("ENTER THE POPULATION OF INDIA(IN MILLION): "))
+EDUCATION_INDEX = (input("ENTER THE INDEX PF EDUCATION: "))
+#we are able to create the inputs by doing string because values are pass on.
+print("NOW WE ARE GOING TO ENTER THE BASIC DATA INPUT AND DATA PROCESSING")
+print("a = " + str(a))
+print("salary = " + str(salary))
+print("POPULATION_INDIA = " + str(POPULATION_INDIA))
+print("EDUCATION_INDEX = " + str(EDUCATION_INDEX))
