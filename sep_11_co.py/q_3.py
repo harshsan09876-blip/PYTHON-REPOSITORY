24fbd9f76ba60e3205b9dea9a159671e1c1da90d
Harsh Chauhan <harshsan09876@gmail.com>
2026-09-11 00:42:10 +0530

    lists, and slices

---
+++ sep_11_co.py/q_3.py
@@ -0,0 +1,9 @@
+#LISTS, SLICES, TUPLE AND DICTIONARIES
+wire = "YUKI TSUKIMO IS A KEY JJK SORCERER"
+print(wire[0:8])
+print(wire[2::])
+print(wire[::4])
+print(wire[:-2])
+print(wire[2:])
+
+# string[start:stop:step]
