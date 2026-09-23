3aeb2e16138c33b6bb214fb75bc58974e234c21a
Harsh Chauhan <harshsan09876@gmail.com>
2026-09-24 00:28:34 +0530

    add the file

--- sep_17_co.py
+++ sep_17_co.py
@@ -1,263 +1,268 @@
-# linked list
+# # # # linked list
 
 
-# class LUCY:
-#     def __init__(self, data, next=None):
-#         self.data = data
-#         self.next = next
+# # # # class LUCY:
+# # # #     def __init__(self, data, next=None):
+# # # #         self.data = data
+# # # #         self.next = next
 
 
-# current1 = Node(10)
+# # # # current1 = Node(10)
 
-# while current1 is not None:
-#     print(current1.data)
-#     current1 = current1.next
+# # # # while current1 is not None:
+# # # #     print(current1.data)
+# # # #     current1 = current1.next
     
     
-class Node:
-        def __init__(self,  data, next=None): 
-            # init se functio run automatic
-            # self kivalue khud ko refer 
-            # data uske andar stored value
-            # next --> none 
-            self.data = data
-            self.next = next
-            #input the node
-            # 
-a = Node(10)
-b = Node(20)
-c = Node(30)
+# # # class Node:
+# # #         def __init__(self,  data, next=None): 
+# # #             # init se functio run automatic
+# # #             # self kivalue khud ko refer 
+# # #             # data uske andar stored value
+# # #             # next --> none 
+# # #             self.data = data
+# # #             self.next = next
+# # #             #input the node
+# # #             # 
+# # # a = Node(10)
+# # # b = Node(20)
+# # # c = Node(30)
             
-            #now connect the node fully pledge
-a.next = b
-b.next = c
+# # #             #now connect the node fully pledge
+# # # a.next = b
+# # # b.next = c
             
-current = a 
-            #isko store karbe ke liye variable
-while current is not None:
-                print(current.data)
+# # # current = a 
+# # #             #isko store karbe ke liye variable
+# # # while current is not None:
+# # #                 print(current.data)
                 
-                current = current.next
+# # #                 current = current.next
             
             
-#now new node add karna hai. 
-class Natsu_DRAGNEEL:
-    def __init__(self,  data, next=None) -> None:
-            self.value = data
-            self.next = None
+# # # #now new node add karna hai. 
+# # # class Natsu_DRAGNEEL:
+# # #     def __init__(self,  data, next=None) -> None:
+# # #             self.value = data
+# # #             self.next = None
             
             
-            #node ko define karo
-a = Node(10)
-b = Node(20)
-c = Node(30)
-d = Node(40)
+# # #             #node ko define karo
+# # # a = Node(10)
+# # # b = Node(20)
+# # # c = Node(30)
+# # # d = Node(40)
             
 
-a.next = b
-b.next = c
-c.next = d
+# # # a.next = b
+# # # b.next = c
+# # # c.next = d
 
 
-while a is not None:
-    print(a.data)
+# # # while a is not None:
+# # #     print(a.data)
     
-    a = a.next
+# # #     a = a.next
 
-# ab do lists ko ek saath traverse karna hai
-#toh hume kya karna hai sabse pehle:
-class GRAY_FULLBUSTER:
-    def __init__(self, data, next=None):
-           self.value = data
-           self.next = None
+# # # # ab do lists ko ek saath traverse karna hai
+# # # #toh hume kya karna hai sabse pehle:
+# # # class GRAY_FULLBUSTER:
+# # #     def __init__(self, data, next=None):
+# # #            self.value = data
+# # #            self.next = None
            
-    a = Node(16)
-    b = Node(26)
-    c = Node(36)
-    d = Node(46)
-    x1 = Node(29)
-    x2 = Node(39)
-    x3 = Node(49)
-    x4 = Node(59)
-    a.next = b
-    b.next = c
-    c.next = d
-    x1.next = x2
-    x2.next = x3
-    x3.next = x4
-    current1 = a
-    current2 = x1
+# # #     a = Node(16)
+# # #     b = Node(26)
+# # #     c = Node(36)
+# # #     d = Node(46)
+# # #     x1 = Node(29)
+# # #     x2 = Node(39)
+# # #     x3 = Node(49)
+# # #     x4 = Node(59)
+# # #     a.next = b
+# # #     b.next = c
+# # #     c.next = d
+# # #     x1.next = x2
+# # #     x2.next = x3
+# # #     x3.next = x4
+# # #     current1 = a
+# # #     current2 = x1
     
-    carry = 0 
-    dummy = Node(0)
-    tail = dummy 
-    while current1 is not None and current2 is not None:
+# # #     carry = 0 
+# # #     dummy = Node(0)
+# # #     tail = dummy 
+# # #     while current1 is not None and current2 is not None:
         
-        print(current1.data, "+" ,current2.data)
-        sum = current1.data + current2.data + carry
-        result_digit = sum % 10
-        carry = sum // 10
+# # #         print(current1.data, "+" ,current2.data)
+# # #         sum = current1.data + current2.data + carry
+# # #         result_digit = sum % 10
+# # #         carry = sum // 10
         
         
         
-        print(sum)
-        print("RESULT_DIGIT: ", result_digit)
-        print("CARRY:  ", carry)
+# # #         print(sum)
+# # #         print("RESULT_DIGIT: ", result_digit)
+# # #         print("CARRY:  ", carry)
                 
-        new_node = Node(result_digit)
-        tail.next = new_node
-        tail = new_node
-        current1 = current1.next
-        current2 = current2.next
+# # #         new_node = Node(result_digit)
+# # #         tail.next = new_node
+# # #         tail = new_node
+# # #         current1 = current1.next
+# # #         current2 = current2.next
         
-    current = dummy.next
+# # #     current = dummy.next
         
-    while current is not None:
-            print(current.data)
-            current = current.next
-        
-        
-        # mein carry ka concept seekh raha tha kaise linked list mein caary and digit alakh rakhni hai . 
-        # result_digit = current//10
-        # result_digit1 = current%10
-        # logic
-        # result_digit1 = sum % 10
-        # result_carry1 = sum // 10
-        
-        # print(result_digit1)
+# # #     while current is not None:
+# # #             print(current.data)
+# # #             current = current.next
         
         
-    #     #leetcode final question
-    # class solution(object):
-    #     def addTwoNumbers(self,l1, l2):
-    #         data = l1
-    #         l2 = None
+# # #         # mein carry ka concept seekh raha tha kaise linked list mein caary and digit alakh rakhni hai . 
+# # #         # result_digit = current//10
+# # #         # result_digit1 = current%10
+# # #         # logic
+# # #         # result_digit1 = sum % 10
+# # #         # result_carry1 = sum // 10
+        
+# # #         # print(result_digit1)
+        
+        
+# # #         #leetcode final question
+# # #     # class solution(object):
+# # #     #     def addTwoNumbers(self,l1, l2):
+# # #     #         data = l1
+# # #     #         l2 = None
             
-    #         self.value = l1
-    #         self.next = l2
-    #         current1 = 0
-    #         current2 = 0
-    #         #write a newnode for sure
+# # #     #         self.value = l1
+# # #     #         self.next = l2
+# # #     #         current1 = 0
+# # #     #         current2 = 0
+# # #     #         #write a newnode for sure
             
-    #         object = Node
-    #         a = object(2)
-    #         b = object(4)
-    #         c = object(3)
+# # #     #         object = Node
+# # #     #         a = object(2)
+# # #     #         b = object(4)
+# # #     #         c = object(3)
             
-    #         a.next = b
-    #         b.next = c
+# # #     #         a.next = b
+# # #     #         b.next = c
             
-    #         s = object(5)
-    #         v = object(6)
-    #         j = object(4)
+# # #     #         s = object(5)
+# # #     #         v = object(6)
+# # #     #         j = object(4)
             
-    #         s.next = v
-    #         v.next = j
+# # #     #         s.next = v
+# # #     #         v.next = j
             
-    #         a = current1
-    #         s = current2
+# # #     #         a = current1
+# # #     #         s = current2
             
-    #         print(current1)
-    #         print(current2)
+# # #     #         print(current1)
+# # #     #         print(current2)
             
-    #         carry = 0
-    #         while current1 is not l1 and current2 is not l2:
-    #             sum = current1 + current2 + carry
+# # #     #         carry = 0
+# # #     #         while current1 is not l1 and current2 is not l2:
+# # #     #             sum = current1 + current2 + carry
                 
-    #             result_digit = sum % 10 
-    #             carry_digit = sum / 10
+# # #     #             result_digit = sum % 10 
+# # #     #             carry_digit = sum / 10
                 
-    #             print("\nnum_sum: ", sum)
-    #             print("\nresult_digit: ", result_digit)
-    #             print("\ncarry_digit: ", carry_digit)
+# # #     #             print("\nnum_sum: ", sum)
+# # #     #             print("\nresult_digit: ", result_digit)
+# # #     #             print("\ncarry_digit: ", carry_digit)
                 
-    #         # we have to print the sum of node now
-    #         dummy = Node(sum)
-    #         tail = dummy
-    #         tail.next = 
+# # #     #         # we have to print the sum of node now
+# # #     #         dummy = Node(sum)
+# # #     #         tail = dummy
+# # #     #         tail.next = 
     
-    # class solution(object):
-    #     def addTwoNumbers(self, l1, l2):
-    #         self.value = l1
-    #         self.next = l2
+# # #     class solution(object):
+# # #         def addTwoNumbers(self, l1, l2):
+# # #             self.value = l1
+# # #             self.next = l2
 
-    #         a = Node(2)
-    #         b = Node(4)
-    #         c = Node(3)
+# # #             a = Node(2)
+# # #             b = Node(4)
+# # #             c = Node(3)
 
-    #         a.next = b
-    #         b.next = c
+# # #             a.next = b
+# # #             b.next = c
 
-    #         current1 = a
+# # #             current1 = a
 
-    #         w = Node(5)
-    #         x = Node(6)
-    #         y = Node(4)
+# # #             w = Node(5)
+# # #             x = Node(6)
+# # #             y = Node(4)
 
-    #         w.next = x
-    #         x.next = y
+# # #             w.next = x
+# # #             x.next = y
 
-    #         current2 = w
+# # #             current2 = w
 
-    #         carry = 0
-    #         while current1 is not None and current2 is not None:
-    #             total = current1.data + current2.data + carry
-    #             result_digit = total % 10
-    #             carry = total // 10
+# # #             carry = 0
+# # #             while current1 is not None and current2 is not None:
+# # #                 total = current1.data + current2.data + carry
+# # #                 result_digit = total % 10
+# # #                 carry = total // 10
 
-    #             print(result_digit, carry)
+# # #                 print(result_digit, carry)
 
-    #             current1 = current1.next
-    #             current2 = current2.next
+# # #                 current1 = current1.next
+# # #                 current2 = current2.next
                 
-    #             print(total)
+# # #                 print(total)
                 
-    #             dummy = ListNode[0]
-    #             tail = dummy
-    #             tail.next = NewNode[0]\
-    #             print(dummy)
+# # #                 dummy = ListNode[0]
+# # #                 tail = dummy
+# # #                 tail.next = NewNode[0]\
+# # #                 print(dummy)
     
     
-    # class structure(object):
-    #     def addTwoNumbers(self, l1, l2):
-    #         self.l1 = l1
-    #         self.l2 = l2
+# # #     class structure(object):
+# # #         def addTwoNumbers(self, l1, l2):
+# # #             self.l1 = l1
+# # #             self.l2 = l2
             
-    #         a = Node[2]
-    #         b = Node[5]
-    #         c = Node[3]
+# # #             a = Node[2]
+# # #             b = Node[5]
+# # #             c = Node[3]
             
-    #         a.next = b
-    #         b.next = c
-    #         current1 = a
+# # #             a.next = b
+# # #             b.next = c
+# # #             current1 = a
             
-    #         x1 = Node[5]
-    #         x2 = Node[6]
-    #         x3 = Node[4]
-    #         x1.l2 = x2
-    #         x2.l2 = x3
+# # #             x1 = Node[5]
+# # #             x2 = Node[6]
+# # #             x3 = Node[4]
+# # #             x1.l2 = x2
+# # #             x2.l2 = x3
             
-    #         current2 = x1
-    #         carry = 0
-    #         while current1 is not None and current2 is not None:
-    #             total + a.l1 + b.l2  
+# # #             current2 = x1
+# # #             carry = 0
+# # #             while current1 is not None and current2 is not None:
+# # #                 total + a.l1 + b.l2  
                 
-    #             result_digit = sum % 10
-    #             carry_digit = sum // 10
+# # #                 result_digit = sum % 10
+# # #                 carry_digit = sum // 10
                 
-    #             print("carry_digit":,  carry_digit)
-    #             print("result_digit":, result_digit)
-    #             print(total)
+# # #                 print("carry_digit":,  carry_digit)
+# # #                 print("result_digit":, result_digit)
+# # #                 print(total)
                 
-    #             dummy = NewNode[result_digit]
-    #             tail = dummy
-    #             tail.next = NewNode
-    #             print(dummy)
+# # #                 dummy = NewNode[result_digit]
+# # #                 tail = dummy
+# # #                 tail.next = NewNode
+# # #                 print(dummy)
             
                 
                 
                 
-                
+# # class solution(object):
+# #     def addTwoNumbers(self, data, next=None):
+# #         self.value = data
+# #         self.next = None
+        
+# #     a = object(10)           
             
             
                 
@@ -276,4 +281,237 @@ class GRAY_FULLBUSTER:
       
 
             
-        
+# #         class Node:
+# #     def __init__(self, data, next=None):
+# #         self.value = data
+# #         self.next = next
+
+
+# # # First Linked List
+# # a = Node(10)
+# # b = Node(20)
+# # c = Node(30)
+
+# # # a.next = 
+# # b.next = c
+
+
+# # # Second Linked List
+# # x2 = Node(30)
+# # x3 = Node(89)
+# # x4 = Node(70)
+
+# # x2.next = x3
+# # x3.next = x4
+
+
+# # current1 = a
+# # current2 = x2
+
+# # # Result list
+# # result_head = None
+# # result_tail = None
+
+# # carry = 0
+
+# # while current1 is not None and current2 is not None:
+
+# #     add = current1.value + current2.value + carry
+
+# #     result_digit = add % 10
+# #     carry = add // 10
+
+# #     # New node
+# #     new_node = Node(result_digit)
+
+# #     # First result node
+# #     if result_head is None:
+# #         result_head = new_node
+# #         result_tail = new_node
+# #     else:
+# #         result_tail.next = new_node
+# #         result_tail = new_node
+
+# #     current1 = current1.next
+# #     current2 = current2.next
+
+
+# # # Remaining carry
+# # if carry != 0:
+# #     new_node = Node(carry)
+# #     result_tail.next = new_node
+# #     result_tail = new_node
+
+
+# # # Display result
+# # current = result_head
+
+# # while current is not None:
+# #     print(current.value, end=" → ")
+# #     current = current.next
+
+# # print("None")
+
+
+
+# class Node:
+#     def __init__(self, data, next=None):
+#         self.value = data
+#         self.next = next
+
+
+# # First Linked List
+# a = Node(10)
+# b = Node(20)
+# c = Node(30)
+
+# a.next = b
+# b.next = c
+
+
+# # Second Linked List
+# x2 = Node(30)
+# x3 = Node(89)
+# x4 = Node(70)
+
+# x2.next = x3
+# x3.next = x4
+
+
+# current1 = a
+# current2 = x2
+
+# # Result list
+# result_head = None
+# result_tail = None
+
+# carry = 0
+
+# while current1 is not None and current2 is not None:
+
+#     add = current1.value + current2.value + carry
+
+#     result_digit = add % 10
+#     carry = add // 10
+
+#     # New node
+#     new_node = Node(result_digit)
+
+#     # First result node
+#     if result_head is None:
+#         result_head = new_node
+#         result_tail = new_node
+#     else:
+#         assert result_tail is not None
+#         result_tail.next = new_node
+#         result_tail = new_node
+
+#     current1 = current1.next
+#     current2 = current2.next
+
+
+# # Remaining carry
+# if carry != 0:
+#     new_node = Node(carry)
+#     if result_tail is None:
+#         result_head = new_node
+#         result_tail = new_node
+#     else:
+#         result_tail.next = new_node
+#         result_tail = new_node
+
+
+# # Display result
+# current = result_head
+
+# while current is not None:
+#     print(current.value, end=" → ")
+#     current = current.next
+
+# print("None")
+
+class Node:
+    def __init__(self, data, next=None):
+        self.value = data
+        self.next = next
+
+
+# First Linked List
+a = Node(16)
+b = Node(26)
+c = Node(36)
+d = Node(46)
+
+a.next = b
+b.next = c
+c.next = d
+
+
+# Second Linked List
+x1 = Node(29)
+x2 = Node(39)
+x3 = Node(49)
+x4 = Node(59)
+
+x1.next = x2
+x2.next = x3
+x3.next = x4
+
+
+# Current pointers
+current1 = a
+current2 = x1
+
+
+# Carry
+carry = 0
+
+
+# Dummy node for result list
+dummy = Node(0)
+tail = dummy
+
+
+# Addition
+while current1 is not None and current2 is not None:
+
+    print(current1.value, "+", current2.value)
+
+    total = current1.value + current2.value + carry
+
+    result_digit = total % 10
+    carry = total // 10
+
+    print("TOTAL:", total)
+    print("RESULT DIGIT:", result_digit)
+    print("CARRY:", carry)
+
+    # New node
+    new_node = Node(result_digit)
+
+    # Add new node to result list
+    tail.next = new_node
+    tail = new_node
+
+    # Move pointers
+    current1 = current1.next
+    current2 = current2.next
+
+
+# If carry is remaining
+if carry != 0:
+    new_node = Node(carry)
+    tail.next = new_node
+    tail = new_node
+
+
+# Display result
+current = dummy.next
+
+print("\nResult Linked List:")
+
+while current is not None:
+    print(current.value, end=" → ")
+    current = current.next
+
+print("None")
